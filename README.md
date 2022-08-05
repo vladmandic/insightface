4016f50e85e3824586f83c7eedbd49ad816214d9
Vladimir Mandic <mandic00@live.com>
2022-08-05 10:47:43 -0400

    demo compare

--- README.md
+++ README.md
@@ -2,29 +2,36 @@
 
 TFJS port of InsightFace
 
-**Work-in-Progress, release expected soon (demo and compare app and model weights)**
 ## Models
 
 Repository contains pretrained **TFJS graph models** for the following **InsightFace** variations  
-Models have been quantized to **F16** for size  
-All models take [1, 112, 112, 3] cropped image of a face as input and produce as single float array as output which represents face embedding
 
-- `insightface-mobilenet-emore`
-- `insightface-mobilenet-swish`
-- `insightface-efficientnet-b0`
-- `insightface-ghostnet-strides1`
-- `insightface-ghostnet-strides2`
+- `human-faceres`: *included for reference*, 6.7MB weights
+- `insightface-mobilenet-emore`: 6.7MB weights, 1.6ms avg
+- `insightface-mobilenet-swish`: 12MB weights, 3.0ms avg
+- `insightface-ghostnet-strides1`: 7.8MB weighs, 9.3ms avg
+- `insightface-ghostnet-strides2`: 7.7MB weights, 7.4ms avg
+- `insightface-efficientnet-b0`: 13MB weights, 9.8ms avg
+
+### Notes
+- Models have been quantized to **F16** for size  
+- All models take `[1, 112, 112, 3]` cropped and normalized `[0..1]` image of a face as input  
+  and produce as single float array as output which represents face embedding  
+- Performance numbers are using RTX3060  
 
 ## Demo & Compare
 
-Demo app in `/src` uses `Human` library to detect and crop faces before running `InsightFace` models  
-as well as using optimized distance methods present in `Human` to measure face similarities given detected face descriptors (embeddings)  
+Demo app in `/src` uses `Human` library to detect and crop faces from input images before running `InsightFace` models to calculate face descriptors (embeddings)  
 
 And yes, you can use any other face detection method  
 
-### Samples
+### Sample Images
 
 GitHub repository is void of sample images, beforing running demo place any number of images of any resolution containing one or more faces into `/assets/samples/`
+
+## Sample Screenshot
+
+![screenshot](assets/screenshot.jpg)
 ### Run
 
 Use built-in dev server to compile sources and start a web server:
@@ -38,5 +45,5 @@ Use built-in dev server to compile sources and start a web server:
 
 ## TBD
 
-- Calculate alternate embeddings
-- Enable switching method between embeddings
+- Optimize `similarity` in (%) from raw `distance`
+- Find best `face.scale` per model
--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,7 @@ import * as tf from '@tensorflow/tfjs';
 import { Human, Config, Result, FaceResult } from '@vladmandic/human/dist/human.esm-nobundle';
 import { log } from './log';
 
-const modelUrls: Array<string> = [
-  'models/insightface-efficientnet-b0.json',
-  'models/insightface-ghostnet-strides1.json',
-  'models/insightface-ghostnet-strides2.json',
-  'models/insightface-mobilenet-emore.json',
-  'models/insightface-mobilenet-swish.json',
-];
-const models: Array<tf.GraphModel> = [];
-
-/*
-  for (const url of modelUrls) {
-    const model = await tf.loadGraphModel(url);
-    models.push(model);
-  }
-  await human.load();
-  await human.warmup();
-}
-
-window.onload = main;
-*/
+const models: Array<{ name: string, model: tf.GraphModel, perf: number, i: number }> = [];
 
 const humanConfig: Partial<Config> = { // user configuration for human, used to fine-tune behavior
   backend: 'webgl' as const,
@@ -30,7 +11,16 @@ const humanConfig: Partial<Config> = { // user configuration for human, used to 
   debug: false,
   modelBasePath: 'node_modules/@vladmandic/human/models',
   filter: { enabled: false, equalization: false, flip: false },
-  face: { enabled: true, detector: { return: true, rotation: true, maxDetected: 50, iouThreshold: 0.01, minConfidence: 0.2 }, mesh: { enabled: true }, attention: { enabled: false }, iris: { enabled: false }, description: { enabled: true }, emotion: { enabled: false } },
+  face: { enabled: true,
+    detector: { return: true, rotation: true, maxDetected: 50, iouThreshold: 0.01, minConfidence: 0.2 },
+    // @ts-ignore hidden param
+    scale: 1.4,
+    mesh: { enabled: true },
+    attention: { enabled: false },
+    iris: { enabled: false },
+    description: { enabled: true },
+    emotion: { enabled: false },
+  },
   body: { enabled: false },
   hand: { enabled: false },
   object: { enabled: false },
@@ -53,6 +43,15 @@ function title(msg) {
   (document.getElementById('title') as HTMLDivElement).innerHTML = msg;
 }
 
+function distance(descriptor1: number[], descriptor2: number[]) { // eucliedan distance
+  let sum = 0;
+  for (let i = 0; i < descriptor1.length; i++) {
+    const diff = descriptor1[i] - descriptor2[i];
+    sum += diff * diff;
+  }
+  return Math.sqrt(sum);
+}
+
 async function selectFaceCanvas(face: Face) {
   // loop through all canvases that contain faces
   const canvases = document.getElementsByClassName('face');
@@ -61,21 +60,22 @@ async function selectFaceCanvas(face: Face) {
   for (const canvas of Array.from(canvases) as HTMLCanvasElement[]) {
     // calculate similarity from selected face to current one in the loop
     const current = all[canvas['tag'].sample][canvas['tag'].face];
-    const similarity = human.similarity(face.embedding, current.embedding);
-    canvas['tag'].similarity = similarity;
+    // const similarity = human.similarity(face.embedding, current.embedding);
+    const dist = distance(face.embedding, current.embedding);
+    canvas['tag'].distance = dist;
     await human.tf.browser.toPixels(current.tensor, canvas);
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
     ctx.font = 'small-caps 1rem "CenturyGothic"';
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(`${(100 * similarity).toFixed(1)}%`, 3, 23);
+    ctx.fillText(`distance ${(dist).toFixed(2)}`, 3, 23);
     ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-    ctx.fillText(`${(100 * similarity).toFixed(1)}%`, 4, 24);
+    ctx.fillText(`distance ${(dist).toFixed(2)}`, 4, 24);
     ctx.fillText(`${current.age}y ${(100 * (current.genderScore || 0)).toFixed(1)}% ${current.gender}`, 4, canvas.height - 6);
   }
   // sort all faces by similarity
   const sorted = document.getElementById('faces') as HTMLDivElement;
   [...Array.from(sorted.children)]
-    .sort((a, b) => parseFloat(b['tag'].similarity) - parseFloat(a['tag'].similarity))
+    .sort((a, b) => parseFloat(a['tag'].distance) - parseFloat(b['tag'].distance))
     .forEach((canvas) => sorted.appendChild(canvas));
   title('faces sorted by similarity');
 }
@@ -134,16 +134,9 @@ async function analyzeImage(index, image, length) {
   });
 }
 
-async function main() {
-  log({ tf: tf.version_core, human: human.version, backend: tf.getBackend() });
-  title('loading models');
-  await human.load();
-  for (const url of modelUrls) models.push(await tf.loadGraphModel(url));
-  // @ts-ignore private property
-  log({ loaded: models.map((model) => model.modelUrl) });
-
+async function loadImages(url: string) {
   title('enumerating input images');
-  const res = await fetch('../assets/samples');
+  const res = await fetch(url);
   const dir: string[] = (res && res.ok) ? await res.json() : [];
   const images: Array<string> = dir.filter((img) => img.endsWith('.jpg'));
 
@@ -152,7 +145,69 @@ async function main() {
   const t1 = human.now();
   const faces = all.reduce((prev, curr) => prev + curr.length, 0);
   log({ images: all.length, faces, time: Math.round(t1 - t0) });
-  title(`extracted ${faces} faces from ${all.length} images in ${Math.round(t1 - t0)} ms`);
+  title(`extracted | ${faces} faces from ${all.length} images in ${Math.round(t1 - t0)} ms`);
+}
+
+function switchEmbeddings(model: string) {
+  for (const img of all) {
+    for (const face of img) face.embedding = face.embeddings[model];
+  }
+  log({ embeddings: model });
+}
+
+async function analyzeEmbeddings() {
+  title(`calculating embeddings | ${models.length} models`);
+  for (let i = 0; i < all.length; i++) {
+    for (const face of all[i]) {
+      const resize = tf.image.resizeBilinear(face.tensor as unknown as tf.Tensor3D, [112, 112]);
+      const batch = tf.expandDims(resize, 0);
+      for (const model of models) {
+        const t0 = performance.now();
+        const res = model.model.execute(batch) as tf.Tensor;
+        if (model.i > 0) model.perf += (performance.now() - t0);
+        model.i++;
+        face.embeddings[model.name] = Array.from(await res.data());
+      }
+      tf.dispose([resize, batch]);
+    }
+    title(`calculating embeddings | ${Math.round(100 * i / all.length)}% [${i}/${all.length}]`);
+  }
+  let total = { ms: 0, i: 0, html: '<option value="human-faceres" default>human-faceres</option>' };
+  for (const model of models) {
+    log({ model: model.name, total: model.perf, avg: model.perf / (model.i - 1), count: model.i });
+    total = {
+      ms: total.ms + model.perf,
+      i: total.i + model.i,
+      html: total.html + `<option value="${model.name}">${model.name}</option>`,
+    };
+  }
+  const select = (document.getElementById('embeddings') as HTMLSelectElement);
+  select.innerHTML = total.html;
+  select.onchange = () => switchEmbeddings(select.value);
+  title(`calculated embeddings | ${all.reduce((prev, curr) => prev + curr.length, 0)} faces in ${Math.round(total.ms)} ms`);
+}
+
+async function loadModels(url: string) {
+  title('enumerating input images');
+  const res = await fetch(url);
+  const dir: string[] = (res && res.ok) ? await res.json() : [];
+  const modelUrls: Array<string> = dir.filter((m) => m.endsWith('.json'));
+  for (const m of modelUrls) {
+    const name = m.replace(/^.*[\\/]/, '').replace('.json', '');
+    const model = await tf.loadGraphModel(m);
+    models.push({ name, model, perf: 0, i: 0 });
+  }
+  // @ts-ignore private property
+  log({ models: models.map((model) => model.modelUrl) });
+}
+
+async function main() {
+  log({ tf: tf.version_core, human: human.version, backend: tf.getBackend() });
+  title('loading models');
+  await human.load();
+  await loadModels('../models');
+  await loadImages('../assets/samples');
+  await analyzeEmbeddings();
 }
 
 window.onload = main;
